c2c2c1b43ead3806f6eb994c887e70556e4885c6
Uchenna ogbu <ogbu41@gmail.com>
2025-10-05 08:43:14 +0300

    debuging

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
@@ -8,15 +7,3 @@ createRoot(document.getElementById("root")).render(
     <App />
   </StrictMode>
 );
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
->>>>>>> 201457c8998a1cde69833fa728881b052315a859
